11f002be8ecc990c13426e1dc854b97cc31e4b5a
SrMonta <jorge.alvamonta@gmail.com>
2022-10-11 13:27:30 +0000

    Connection test 2

--- client/src/app/services/hello.service.ts
+++ client/src/app/services/hello.service.ts
@@ -10,12 +10,13 @@ export class HelloService {
         private httpClient: HttpClient
     ){}
 
-    private url: string = 'https://3001-cloudnativea-hackbffjam-1mw2pl24e0g.ws-eu70.gitpod.io'
+    // private url: string = 'https://3001-cloudnativea-hackbffjam-1mw2pl24e0g.ws-eu70.gitpod.io'
     // private url: string = 'https://localhost:3001'
     // private url: string = 'https://hack-bff-jam-dev-jam-h.cp4d-workshop-hackathon-o-0a90eebd1b04c36c8a65993d6a0c3f93-0000.eu-de.containers.appdomain.cloud'
 
     getHello(): Observable<Object> {
-        return this.httpClient.get(this.url+'/hello?name=carl')
+        // return this.httpClient.get(this.url+'/hello?name=carl')
+        return this.httpClient.get('/api/hello?name=carl')
         // return of(Math.random())
     }
 }
